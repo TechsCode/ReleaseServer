a71f52ba420f6cb7cddc9ebe59862104ef5dc559
Faab007NL <mail@fabiandingemans.nl>
2021-10-13 17:32:16 +0200

    Fixed the release server

--- src/main/java/me/TechsCode/ReleaseServer/GithubReleaseFetcher.java
+++ src/main/java/me/TechsCode/ReleaseServer/GithubReleaseFetcher.java
@@ -7,7 +7,10 @@ import com.google.gson.JsonParser;
 import me.TechsCode.ReleaseServer.objects.Project;
 import org.apache.commons.io.IOUtils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -35,11 +38,26 @@ public abstract class GithubReleaseFetcher extends Thread {
 
             for(Project project : projects){
                 try {
-                    String randomParameter = "?rnd="+random.nextInt();
-                    String accessTokenParameter = project.getGithubToken().isPresent() ? "&access_token="+project.getGithubToken().get() : "";
-                    String url = "https://api.github.com/repos/"+project.getGithubRepository()+"/releases"+randomParameter+accessTokenParameter;
+                    String accessTokenParameter = project.getGithubToken().isPresent() ? project.getGithubToken().get() : "";
+                    String urlString = "https://api.github.com/repos/"+project.getGithubRepository()+"/releases";
 
-                    JsonArray jsonArray = (JsonArray) JsonParser.parseString(IOUtils.toString(new URL(url), StandardCharsets.UTF_8));
+                    URL url = new URL(urlString);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    con.setRequestMethod("GET");
+                    con.setRequestProperty("Authorization", "token "+accessTokenParameter);
+                    con.setRequestProperty("Accept", "application/json");
+
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(con.getInputStream()));
+                    String inputLine;
+                    StringBuilder content = new StringBuilder();
+                    while ((inputLine = in.readLine()) != null) {
+                        content.append(inputLine);
+                    }
+                    in.close();
+                    con.disconnect();
+
+                    JsonArray jsonArray = JsonParser.parseString(content.toString()).getAsJsonArray();
 
                     for(JsonElement jsonElement : jsonArray){
                         JsonObject release = (JsonObject) jsonElement;
@@ -63,6 +81,7 @@ public abstract class GithubReleaseFetcher extends Thread {
                         releases.add(new Release(project, id, name, uniqueTag, description, assets));
                     }
                 } catch (IOException e) {
+                    e.printStackTrace();
                     System.out.println("Could not fetch releases for "+project.getName()+" ("+project.getGithubRepository()+")");
                 }
             }
