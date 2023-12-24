3dfbf143db05b98b425edf26fb38b0edb530a13d
Fabian Dingemans <mail@fabiandingemans.nl>
2023-12-24 16:25:17 +0100

    Changed deploy.sh to use php8.2

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ echo "-----------------------------------"
 # Enter maintenance mode or return true
 # if already is in maintenance mode
 echo "1/12 Entering maintenance mode"
-(php8.1 artisan down) || true
+(php8.2 artisan down) || true
 
 # Stash all changes
 echo "2/12 Stash all changes"
@@ -23,15 +23,15 @@ git stash pop
 
 # Install composer dependencies
 echo "5/12 Install composer dependencies"
-/usr/bin/php8.1 /usr/local/bin/composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+/usr/bin/php8.2 /usr/local/bin/composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 
 # Clear the old cache
 echo "6/12 Clear the old cache"
-php8.1 artisan clear-compiled
+php8.2 artisan clear-compiled
 
 # Recreate cache
 echo "7/12 Recreate cache"
-php8.1 artisan optimize
+php8.2 artisan optimize
 
 # Compile npm assets
 echo "8/12 Install npm dependencies"
@@ -43,17 +43,17 @@ npm run prod
 
 # Run database migrations
 echo "10/12 Run database migrations"
-php8.1 artisan migrate --force
+php8.2 artisan migrate --force
 
 # Recreate cache
 echo "11/12 Recreate cache"
-php8.1 artisan config:cache
-php8.1 artisan route:cache
-php8.1 artisan event:cache
-php8.1 artisan view:cache
+php8.2 artisan config:cache
+php8.2 artisan route:cache
+php8.2 artisan event:cache
+php8.2 artisan view:cache
 
 # Exit maintenance mode
 echo "12/12 Exit maintenance mode"
-php8.1 artisan up
+php8.2 artisan up
 
 echo "Deployment finished!"
